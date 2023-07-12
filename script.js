fa1e5d4b3ccd23b98fd1c03c339947df487fd005
Blaze-Codes <nic.holzmann@web.de>
2023-07-12 22:32:34 +0200

    ES FUNKTIONIERT!!!

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-var slanglate = document.getElementById("slanglate");
-var input = document.getElementById("input");
-var rainbow = rainbow.getElementById("rainbow");
 
-slanglate.onclick = function slangify(){
-    var context = input.value
-    alert(context);
-    console.log("TEST")
-    rainbow.style.color = randomColor();
-}
+var replacements = {
+  "Hallo": ["Hi", "Hey", "Guten Tag"],
+  "Welt": ["Globus", "Erde", "Planet"],
+  // Weitere Wortersetzungspaare hier definieren
+};
 
-function randomColor() {
-    var s = '123456789ABCDEF';
-    var color = '#';
+var additionalReplacements = ["Assi", "bodenlos", "cringe", "dayum", "Ehrenmann", "flex", "Gommemode", "haram", "it's a prank bro", "Jungfrau", "krass", "Luder", "muss los", "noice", "Opfer", "pass", "Quatschtempel", "reudig", "slay", "toxic", "unter aller Würde", "vallah", "Weichei", "xD, icks deh", "YOLO!", "Zickenstall"];
 
-    for (var i = 0; i < 6; i++) {
-        color = color + s[Math.floor(Math.random() * 10)]
-    }
+function slanglate() {
+  var input = document.getElementById('input').value; // Wert des Input-Feldes abrufen
+  var words = input.split(' '); // Eingabe in ein Array von Wörtern aufteilen
 
-    return color;
+  var modifiedWords = words.map(function(word) {
+    // Wort aus dem zusätzlichen Array verwenden
+    var randomIndex = Math.floor(Math.random() * additionalReplacements.length);
+    return additionalReplacements[randomIndex];
+  });
+
+  var modifiedText = modifiedWords.join(' ');
+
+  console.log(modifiedText); // Den Text mit den ersetzen Wörtern anzeigen
+  document.getElementById("output").value = modifiedText;
 }
