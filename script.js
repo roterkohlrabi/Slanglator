d6993c2916c51c3c2d5482dce088bdeffe82d96d
Blaze-Codes <nic.holzmann@web.de>
2023-07-12 21:19:28 +0200

    abcdefg

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 var slanglate = document.getElementById("slanglate");
+var input = document.getElementById("input");
+var rainbow = rainbow.getElementById("rainbow");
 
 slanglate.onclick = function slangify(){
-    alert("BRUUUDA");
+    var context = input.value
+    alert(context);
     console.log("TEST")
+    rainbow.style.color = randomColor();
+}
+
+function randomColor() {
+    var s = '123456789ABCDEF';
+    var color = '#';
+
+    for (var i = 0; i < 6; i++) {
+        color = color + s[Math.floor(Math.random() * 10)]
+    }
+
+    return color;
 }
